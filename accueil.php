1eaa9d936fed5f99465b02bf3a814e362153c95b
zaza-rgb <ramatoubatogouma@gmail.com>
2026-04-13 15:19:00 +0000

    page accueil du projet

---
+++ accueil.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>                                                                     
+       
+        body{
+            background-color: #f4f4f4;
+            
+        }
+        h2{
+           color :;
+           text-align: center ; 
+           font-style: italic normal;
+           
+        }
+
+    </style>
+</head>
+<body>
+    <?php
+    include("./include/header.php")
+    ?>
+    <div>
+    <h2>BIENVENU DANS ALLSHOP</h2>
+    </div>
+    <?php
+    include("./include/footer.php")
+    ?>
+</body>
+</html>
