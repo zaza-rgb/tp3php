6e3df74c4c9ab905c370d3fbcb0bc04d78369db4
uzumakijoel <joelnambigue360@gmail;com>
2026-04-16 10:55:58 +0000

    commander finalisation

--- panier.php
+++ panier.php
@@ -13,7 +13,8 @@
     require 'detail.php';
 
     session_start();
-    if (!isset($_POST['vider'])) {
+    $tota=0;
+    if (!isset($_POST['vider']) && !isset($_POST['commander'])) {
     $id = $_POST['idprod'];
     $quantite = (int)$_POST['qte'];
 
@@ -31,7 +32,43 @@
                     }
     }else if (isset($_POST['vider'])) {
         unset($_SESSION['panier']);
-    }
+        }else if (isset($_POST['commander'])) {
+            if(isset($_SESSION['ref_uti'])){ 
+
+                $ref_uti = $_SESSION['ref_uti'];
+                $total=0;
+                $date = date("Y-m-d H:i:s");
+                foreach ($_SESSION['panier'] as $idprod => $qte) {
+
+                    $sql = "SELECT prix FROM produit WHERE idprod = ?";
+                    $stmt = $com->prepare($sql);
+                    $stmt->execute([$idprod]);
+                    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                    $total+=$row['prix']*$qte;
+                    }
+                $sql = "INSERT INTO commande (id_utilisateur, date_commande, total) VALUES (?, ?, ?)";
+                $stmt = $com->prepare($sql);
+                $stmt->execute([$ref_uti,$date, $total]);
+                $lastId = $com->lastInsertId();
+
+                foreach ($_SESSION['panier'] as $idprod => $qte) {
+
+                    $sql = "SELECT prix FROM produit WHERE idprod = ?";
+                    $stmt = $com->prepare($sql);
+                    $stmt->execute([$idprod]);
+                    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                    $price=$row['prix'];
+                    $sql = "INSERT INTO commande_produit (id_commande,id_produit, quantite,prix_unitaire) VALUES (?, ?, ?, ?)";
+                    $stmt = $com->prepare($sql);
+                    $stmt->execute([$lastId, $idprod, $qte, $price]);
+
+                    }
+                echo "Achat effectuéé avec succes ,";
+                unset($_SESSION['panier']);
+                }else{
+                    echo "Vous devez vous connecter avant d'effectuer un achat";
+                    }}
+
     if (!empty($_SESSION['panier'])) {
         foreach ($_SESSION['panier'] as $idprod => $qte) {
             $sql = "SELECT * FROM produit WHERE idprod = ?";
@@ -46,7 +83,10 @@
 
                         <div class="vertical">
                             <h5 class="productname"><?php echo htmlspecialchars($row['nomprod']) ?></h5>
-                            <h6 class="prix"><?php echo htmlspecialchars($row['prix']) ?></h6>
+                            <h6 class="prix"><?php echo htmlspecialchars($row['prix']*$qte) ?></h6>
+                            <?php $tot=$row['prix']*$qte;
+                                  $tota+=$tot;
+                            ?>
                         </div>
                     </div>
                     <div class="right">
@@ -64,7 +104,9 @@
         }
             ?>
             <form method="post" action="panier.php">
+            <h6 class="prix">TOTAL= <?php echo $tota; ?> fcfa</h6>
             <button type="submit" name="vider">Vider le panier</button>
+            <button type="submit" name="commander">commander</button>
             </form>
 
            <?php
